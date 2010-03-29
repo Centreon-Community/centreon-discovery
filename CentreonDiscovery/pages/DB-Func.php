fd5176b318de0ccf0284c7d53df026f756340183
teamreon <teamreon>
2010-03-29 19:45:00 +0000

    Fix Bug #1444: La relance de l'auto détection génère des doublons
    git-svn-id: http://svn.modules.centreon.com/centreon-discovery/trunk@51 3b3b1dc1-9441-49e1-a41a-54ccb5f3ab27

--- CentreonDiscovery/pages/DB-Func.php
+++ CentreonDiscovery/pages/DB-Func.php
@@ -60,6 +60,17 @@
 		return $result;
 	}
 	
+	/*  Vérification si la requête retourne une donnée
+	 * @parm : $cmd - Requête
+	*/
+	function queryIsEmpty($cmd) {
+		$empty = 0;
+		$res = mysql_query($cmd);
+		$num_row = mysql_num_rows($res);
+		if ( $num_row > 0 ) { $empty = 1; }
+		return $empty;
+	}
+	
 	/*  Création des services host à partir des Templates de services
 	 * @parm : $host_id - ID de l'host
 	 * @parm : $service_id - ID du template de service
@@ -72,25 +83,37 @@
 			$description = $data['service_description'];
 		}
 		
-		$host_description = mysql_query("SELECT host_alias FROM host WHERE host_id = ".$host_id." LIMIT 1");
+		$host_description = mysql_query("SELECT host_name FROM host WHERE host_id = ".$host_id." LIMIT 1");
 		while ( $data = mysql_fetch_assoc($host_description) ) {
-			$host_alias = $data['host_alias'];
+			$host_name = $data['host_name'];
 		}
 
-		$service_description = $host_alias.'_'.substr($description,9,strlen($description)).'_'.$elt_name;	
-
-		if ( $group_description == "Interface Reseaux" ) {
-			$arguments = '"!$USER2$!'.substr($elt_name,1).'!"';
+		$service_description = $host_name.'_'.substr($description,18,strlen($description)).'_'.$elt_name;
+		
+		if ( queryIsEmpty('SELECT service_id FROM service WHERE service_description = "'.$service_description.'"') == 0 ) {
+			
+			if ( $group_description == "Interface Reseaux" ) {
+				$arguments = '"!$USER2$!'.substr($elt_name,1).'!"';
+			}
+			else if ( $group_description == "Partitions" ) {
+				$arguments = '"!'.$elt_name.'!80!90!$USER2$!1"';
+			}
+			else if ( $group_description == "Processus" ) {
+				$arguments =  '"!$USER2$!'.$elt_name.'!"';
+			}
+			
+			if ( !mysql_query("INSERT INTO service (service_id,service_template_model_stm_id,command_command_id,timeperiod_tp_id,command_command_id2,timeperiod_tp_id2,service_description,service_alias,display_name,service_is_volatile,service_max_check_attempts,service_normal_check_interval,service_retry_check_interval,service_active_checks_enabled,service_passive_checks_enabled,initial_state,service_parallelize_check,service_obsess_over_service,service_check_freshness,service_freshness_threshold,service_event_handler_enabled,service_low_flap_threshold,service_high_flap_threshold,service_flap_detection_enabled,service_process_perf_data,service_retain_status_information,service_retain_nonstatus_information,service_notification_interval,service_notification_options,service_notifications_enabled,service_first_notification_delay,service_stalking_options,service_comment,command_command_id_arg,command_command_id_arg2,service_register,service_activate) VALUES (NULL,".$service_id.",NULL,NULL,NULL,NULL,'".$service_description."',NULL,NULL,'2',NULL,NULL,NULL,'2','2',NULL,'2','2','2',NULL,'2',NULL,NULL,'2','2','2','2',NULL,NULL,'2',NULL,NULL,NULL,".$arguments.",NULL,'1','1')") ) { echo mysql_error(); }
+			if ( !mysql_query("INSERT INTO extended_service_information (esi_id,service_service_id,esi_notes,esi_notes_url,esi_action_url,esi_icon_image,esi_icon_image_alt,graph_id) VALUES (NULL,(SELECT service_id FROM service WHERE service_description = '".$service_description."' LIMIT 1),NULL,NULL,NULL,NULL,NULL,NULL)") ) { echo mysql_error(); }
+			if ( !mysql_query("INSERT INTO host_service_relation (hsr_id,hostgroup_hg_id,host_host_id,servicegroup_sg_id,service_service_id) VALUES (NULL,NULL,".$host_id.",NULL,(SELECT service_id FROM service WHERE service_description = '".$service_description."' LIMIT 1))") ) { echo mysql_error(); }	
+			
+			$result[0] = 0;
+			$result[1] = $service_description;
 		}
-		else if ( $group_description == "Partitions" ) {
-			$arguments = '"!'.$elt_name.'!80!90!$USER2$!1"';
-		}
-		else if ( $group_description == "Processus" ) {
-			$arguments =  '"!$USER2$!'.$elt_name.'!"';
+		else {
+			$result[0] = 1;
+			$result[1] = $service_description;
 		}
 		
-		$service = mysql_query("INSERT INTO service (service_id,service_template_model_stm_id,command_command_id,timeperiod_tp_id,command_command_id2,timeperiod_tp_id2,service_description,service_alias,display_name,service_is_volatile,service_max_check_attempts,service_normal_check_interval,service_retry_check_interval,service_active_checks_enabled,service_passive_checks_enabled,initial_state,service_parallelize_check,service_obsess_over_service,service_check_freshness,service_freshness_threshold,service_event_handler_enabled,service_low_flap_threshold,service_high_flap_threshold,service_flap_detection_enabled,service_process_perf_data,service_retain_status_information,service_retain_nonstatus_information,service_notification_interval,service_notification_options,service_notifications_enabled,service_first_notification_delay,service_stalking_options,service_comment,command_command_id_arg,command_command_id_arg2,service_register,service_activate) VALUES (NULL,".$service_id.",NULL,NULL,NULL,NULL,'".$service_description."',NULL,NULL,'2',NULL,NULL,NULL,'2','2',NULL,'2','2','2',NULL,'2',NULL,NULL,'2','2','2','2',NULL,NULL,'2',NULL,NULL,NULL,".$arguments.",NULL,'1','1')");
-		$extended_service = mysql_query("INSERT INTO extended_service_information (esi_id,service_service_id,esi_notes,esi_notes_url,esi_action_url,esi_icon_image,esi_icon_image_alt,graph_id) VALUES (NULL,(SELECT service_id FROM service WHERE service_description = '".$service_description."' LIMIT 1),NULL,NULL,NULL,NULL,NULL,NULL)");
-		$relation_host_service = mysql_query("INSERT INTO host_service_relation (hsr_id,hostgroup_hg_id,host_host_id,servicegroup_sg_id,service_service_id) VALUES (NULL,NULL,".$host_id.",NULL,(SELECT service_id FROM service WHERE service_description = '".$service_description."' LIMIT 1))");
+		return $result;
 	} 
 ?>
--- CentreonDiscovery/pages/page4.php
+++ CentreonDiscovery/pages/page4.php
@@ -1,20 +1,69 @@
 <?
 	$host_id = $_POST["host_id"];
+	$host_name = $_POST["host_name"];
 	$group_description = $_POST["group_description"];
 	$elt_name = $_POST["elt_name"];
 	$services = $_POST["services"];
 
+	echo '<br><br>'," \n ";
+	echo '	<span style="font-family: Candara; font-weight: bold; font-size: 24px; color: black; font-style: italic;">'," \n ";
+	echo '    <center>'.$host_name.'</center>'," \n ";
+	echo '	</span>'," \n ";
+	echo '<br><br>'," \n ";
+	
+	
+	
 	for($i=0;$i<sizeof($group_description);$i++) {
+	
+	
+		echo '<table class="ListTable">'," \n ";																											
+		echo '	<tr class="ListHeader">'," \n ";
+		echo '		<td class="ListColHeaderCenter" width=30px><INPUT TYPE=CHECKBOX disabled="disabled" CHECKED /></td>'," \n ";
+		echo '		<td class="ListColHeader"><big>'. $group_description[$i] .'</big></td>'," \n ";
+		echo '	</tr>'," \n ";
+	
+	
 		for($j=0;$j<sizeof($elt_name[$i]);$j++) {
+		
+		
+			echo '	<tr class="list_one">'," \n ";
+			echo '  	<td class="ListColCenter"></td>'," \n ";
+			echo ' 		<td class="ListCol">'," \n ";
+			echo '			<table WIDTH=100% BORDER="0" ALIGN="LEFT" VALIGN="MIDDLE" CELLPADDING="0" CELLSPACING="0">'," \n ";
+			echo '				<tr>'," \n ";
+			echo '  				<td WIDTH=30px ALIGN="CENTER">'," \n ";
+			echo '						<img style="border: 0px solid; width: 15px;" alt="-" src="./modules/CentreonDiscovery/pictures/double_puce.png">'," \n ";
+			echo ' 					</td>'," \n ";
+			echo '  				<td><b>' . $elt_name[$i][$j] .' :</b></td>'," \n ";
+
+		
 			for($k=0;$k<sizeof($services[$i][$j]);$k++) {
 				/* TEST */ //echo '<br><p>group_description = '.$group_description[$i].'</p>';
 				/* TEST */ //echo '<p>elt_name = '.$elt_name[$i][$j].'</p>';
 				/* TEST */ //echo '<p>services = '.$services[$i][$j][$k].'</p><br>';
-				InsertionRelationServiceBDD($host_id,$services[$i][$j][$k],$elt_name[$i][$j],$group_description[$i]);
+				
+				$insert = InsertionRelationServiceBDD($host_id,$services[$i][$j][$k],$elt_name[$i][$j],$group_description[$i]);
+				$service_name = substr(strrchr($insert[1], "_"), 1);		
+				echo '				<tr>'," \n ";
+				if ( $insert[0] == 0 ) {
+					echo '					<td class="ListColHeaderCenter" width=30px><img style="border: 0px solid; width: 15px;" alt="-" src="./modules/CentreonDiscovery/pictures/true.png"></td>'," \n ";
+					echo '					<td>'.$service_name.' : ce service est d&eacute;sormais configur&eacute; !</td>'," \n ";
+				}
+				else {
+					echo '					<td class="ListColHeaderCenter" width=30px><img style="border: 0px solid; width: 15px;" alt="-" src="./modules/CentreonDiscovery/pictures/false.png"></td>'," \n ";
+					echo '					<td>'.$service_name.' : ce service existe d&eacute;j&agrave; !</td>'," \n ";
+				}
+				echo '				</tr>'," \n ";
+				echo '				<tr HEIGHT="10px";></tr>'," \n ";
+				echo '			</table>'," \n ";
+				echo '		</td>'," \n ";
+				echo '	</tr>'," \n ";
 			}			
 		}
+		echo '</table>'," \n ";
+		echo '<br>'," \n ";
 	}
-	
+	echo '<br><br>'," \n ";
 	echo '<p>'," \n ";
 	echo '	<span style="font-family: Candara; font-weight: bold; color: red; font-style: italic;">'," \n ";
 	echo '  	<center><big>Votre configuration est d&eacute;sormais appliqu&eacute;e...</center></big>'," \n ";

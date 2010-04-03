caf85470a9fb71db2b4f61990040ae9d3aa246f8
teamreon <teamreon>
2010-04-03 09:20:12 +0000

    GNU GPL Version 3.00
    git-svn-id: http://svn.modules.centreon.com/centreon-discovery/trunk@53 3b3b1dc1-9441-49e1-a41a-54ccb5f3ab27

--- CentreonDiscovery/pages/page4.php
+++ CentreonDiscovery/pages/page4.php
@@ -1,4 +1,26 @@
 <?
+/*  This file is part of CentreonDiscovery.
+ *
+ *	 CentreonDiscovery is developped with GPL Licence 3.0 :
+ *	 Developped by : Jonathan Teste - Cedric Dupre
+ *   
+ *   CentreonDiscovery is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License.
+ *   
+ *   CentreonDiscovery is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with CentreonDiscovery.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ 	// **************************************************************************
+	// * Page 4 : Application de la configuration *
+	// **************************************************************************
+
 	$host_id = $_POST["host_id"];
 	$host_name = $_POST["host_name"];
 	$group_description = $_POST["group_description"];
--- CentreonDiscovery/php/install.php
+++ CentreonDiscovery/php/install.php
@@ -1,9 +1,20 @@
 <?php
-/*
- * Centreon Discover is developped with GPL Licence 2.0 :
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
- * Developped by : Jonathan TESTE 
+/*  This file is part of CentreonDiscovery.
  *
+ *	 CentreonDiscovery is developped with GPL Licence 3.0 :
+ *	 Developped by : Jonathan Teste - Cedric Dupre
+ *   
+ *   CentreonDiscovery is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License.
+ *   
+ *   CentreonDiscovery is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with CentreonDiscovery.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* Fonction PHP */	
